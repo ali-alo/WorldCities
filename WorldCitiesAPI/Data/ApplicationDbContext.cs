3144a9a322a53dae0c14a239ebcc73cf2e778eb4
Ali Alo <alialo3888@gmail.com>
2024-02-17 13:08:25 +0500

    Set up database; add Cities and Countries controllers

---
+++ WorldCitiesAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+using WorldCitiesAPI.Data.Models;
+
+namespace WorldCitiesAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<City> Cities => Set<City>();
+        public DbSet<Country> Countries => Set<Country>();
+
+        public ApplicationDbContext() : base()
+        {
+            
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        { 
+        
+        }
+    }
+}
