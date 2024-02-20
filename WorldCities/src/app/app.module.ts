f2a0cdc7ecc798c9e4759e4e8a729fc9884a9739
Ali Alo <alialo3888@gmail.com>
2024-02-20 15:28:21 +0500

    Add pagination and sorting to cities

--- WorldCities/src/app/app.module.ts
+++ WorldCities/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CitiesComponent } from './cities/cities.component';
+import { AngularMaterialModule } from './angular-material.module';
 
 @NgModule({
   declarations: [
@@ -23,10 +21,8 @@ import { CitiesComponent } from './cities/cities.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- WorldCities/src/app/cities/cities.component.ts
+++ WorldCities/src/app/cities/cities.component.ts
@@ -1,7 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { City } from './city';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { ApiResult } from './apiResult';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-cities',
@@ -9,15 +13,53 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./cities.component.scss']
 })
 export class CitiesComponent implements OnInit {
-  public cities!: City[];
+  private defaultPageSize: number = 10;
+  private defaultPageIndex: number = 0;
+
+  public displayedColumns: string[] = ['id', 'name', 'lat', 'lon'];
+  public cities!: MatTableDataSource<City>;
+  public defaultSortColumn: string = "name";
+  public defaultSortOrder: "asc" | "desc" = "asc";
+  
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.get<City[]>(environment.baseUrl + 'api/Cities')
-      .subscribe({
-        next: (result) => this.cities = result,
-        error: (error) => console.error(error)
-    })
+    this.loadData();
+  }
+
+  loadData(): void {
+    const pageEvent = new PageEvent();
+    pageEvent.pageIndex = this.defaultPageIndex;
+    pageEvent.pageSize = this.defaultPageSize;
+    this.getData(pageEvent);
+  }
+
+  getData(event: PageEvent): void {
+    const url = environment.baseUrl + 'api/Cities';
+    const params = new HttpParams()
+      .set("pageIndex", event.pageIndex.toString())
+      .set("pageSize", event.pageSize.toString())
+      .set("sortColumn", this.defaultSortColumn)
+      .set("sortOrder", (this.sort)
+        ? this.sort.direction
+        : this.defaultSortOrder);
+    this.sendGetRequest(url, params);
+  }
+
+  sendGetRequest(url: string, params: HttpParams): void {
+    this.http.get<ApiResult<City>>(url, { params })
+    .subscribe({
+      next: result => {
+        this.paginator.length = result.totalCount;
+        this.paginator.pageIndex = result.pageIndex;
+        this.paginator.pageSize = result.pageSize;
+        this.cities = new MatTableDataSource<City>(result.data);
+      },
+      error: error => console.error(error)
+    });
   }
 
 }
